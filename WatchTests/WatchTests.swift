890d4bd351d9daa6993a79008d386332ca9f43e6
Al Malki <ASAAM@als-macbook-pro.local>
2015-11-25 18:49:48 +0300

    Initial Commit

---
+++ WatchTests/WatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  WatchTests.swift
+//  WatchTests
+//
+//  Created by Al Malki on 2/13/1437 AH.
+//  Copyright © 1437 ASAAMSOFT. All rights reserved.
+//
+
+import XCTest
+@testable import Watch
+
+class WatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
