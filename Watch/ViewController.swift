5c7a841f5a106ac846da3b11903777e9bd26d884
Al Malki <ASAAM@AsaamSoft.lan>
2015-12-04 20:57:31 +0300

    First Version

--- Watch/ViewController.swift
+++ Watch/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "upadeTime", userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func upadeTime()
+    {
+        
+        let myTime = NSDate()
+        
+        let format = NSDateFormatter()
+        format.dateFormat = "hh:mm:ss a"
+        
+        self.myLable.text = format.stringFromDate(myTime)
+        
+        
+        
+        
+    }
+    
 
 }
 
